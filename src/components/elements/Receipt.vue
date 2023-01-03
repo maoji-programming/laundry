6f24e46b6759e7a97c5a98ea0fab599be1099755
Maoji Programming <maoji.programming@gamil.com>
2023-01-03 18:16:48 +0800

    built dashboard, receipt

---
+++ src/components/elements/Receipt.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="d-flex my-2">
+        <div >
+            <slot name="icon"></slot>
+        </div>
+        <div>
+            <div class="main-tag num">
+                <span class="tag-name"><b>Number</b></span>
+                <div class="tag-content">{{ pendingItem.pendingNumber }}</div>
+            </div>
+        </div>
+        <div class="ms-auto">
+            <div class="main-tag order-time">
+                <span class="tag-name"><b>Start Time</b></span>
+                <div class="tag-content">{{ pendingItem.orderTime }}</div>
+            </div>
+        </div>
+        <div>
+            <!-- more -->
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import {defineComponent, reactive, ref} from "vue";
+    
+    export default defineComponent({
+        props: ['pendingItem']
+    })
+</script>
+
+<style scoped>
+    .main-tag {
+        border: 2px solid #22aaee;
+        
+        border-radius: 10px;
+        align-items: center;
+    }
+
+    .num{
+        width: 100px;
+    }
+
+    .order-time{
+        width: 200px;
+    }
+
+    .tag-name {
+        position: absolute;
+        top: -10px;
+        left: 10px;
+        color: #22aaee;
+        background: #ffffff;
+        font-size: 8px;
+    }
+
+    .tag-content {
+        text-align: center;
+    }
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,42 +1,105 @@
-<script setup>
-import RectButton from '../components/elements/RectButton.vue';
-import RectWindow from '../components/elements/RectWindow.vue';
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-    <b-container fluid>
+    <!-- <b-container fluid>
         <b-row>
             <b-col cols="9">
                 <b-row>
-                    <b-col cols="6">[customer]</b-col>
-                    <b-col cols="6">[profit]</b-col>
+                    <b-col cols="6">
+                        
+                    </b-col>
+                    <b-col cols="6">
+                        
+                    </b-col>
                 </b-row>
                 <b-row>
-                    <RectWindow title="Queue"></RectWindow>
+                    <b-col cols="12">
+                        <RectWindow title="Queue"></RectWindow>                       
+                    </b-col>
                 </b-row>
             </b-col>
             <b-col cols="3">
                 <b-row>
-                    <RectWindow title="Statistic"></RectWindow>
-                </b-row>
-                <b-row>
-                    <b-col cols="6">
-                        <RectButton title="Add"></RectButton>
-                    </b-col>
-                    <b-col cols="6">
-                        <RectButton title="Backend"></RectButton>
+                    <b-col cols="12">
+                        <RectWindow title="Statistic"></RectWindow>
                     </b-col>
                 </b-row>
                 <b-row>
                     <b-col cols="6">
-                        <RectButton title="Print"></RectButton>
+                        <RectButton title="Add" background="#22aaee"></RectButton>
                     </b-col>
                     <b-col cols="6">
-                        <RectButton title="Setting"></RectButton>
+                        <RectButton title="Backend" background="#22aaee"></RectButton>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col cols="6">
+                        <RectButton title="Print" background="#22aaee"></RectButton>
+                    </b-col>
+                    <b-col cols="6">
+                        <RectButton title="Setting" background="#22aaee"></RectButton>
                     </b-col>
                 </b-row>
             </b-col>
         </b-row>
-    </b-container>
+    </b-container> -->
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="row my-2">
+                    <div class="col-6">
+                        <RectWindow title="Profit"></RectWindow>
+                    </div>
+                    <div class="col-6">
+                        <RectWindow title="Number"></RectWindow>
+                    </div>
+                </div>
+                <div class="row my-2">
+                    <div class="col-12">
+                        <RectWindow title="Receipt List" height="400px">
+                            <Receipt :pendingItem="receipts[0]"></Receipt>
+                        </RectWindow>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="row my-2">
+                    <div class="col-12">
+                        <RectWindow title="Statistics"></RectWindow>
+                    </div>
+                </div>
+                <div class="row my-2">
+                    <div class="col-6">
+                        <RectButton title="Add" background="#22aaee"></RectButton>
+                    </div>
+                    <div class="col-6">
+                        <RectButton title="Search" background="#22aaee"></RectButton>
+                    </div>
+                </div>
+                <div class="row my-2">
+                    <div class="col-6">
+                        <RectButton title="Data" background="#22aaee"></RectButton>
+                    </div>
+                    <div class="col-6">
+                        <RectButton title="Setting" background="#22aaee"></RectButton>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+    import Receipt from '../components/elements/Receipt.vue';
+    import RectButton from '../components/elements/RectButton.vue';
+    import RectWindow from '../components/elements/RectWindow.vue';
+    
+    
+                receipts: [
+                    {
+                        pendingNumber: 'A0001',
+                        orderTime: '2022-10-02 22:10:01'
+                    }
+                ]
+            
+    
+
+</script>
