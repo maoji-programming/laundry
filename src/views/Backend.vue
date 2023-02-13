95919c95e9370b679d76ecf2adfbbed3cc005477
Maoji Programming <maoji.programming@gamil.com>
2023-02-13 18:02:58 +0800

    install axios

--- src/views/Backend.vue
+++ src/views/Backend.vue
@@ -2,23 +2,57 @@
   <div class="container">
     <div class="row">
       <div class="col-10">
-          <rect-window title="Customer List"></rect-window>
+          <rect-window :title="title.value">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th ></th>
+                </tr>
+                <tr>
+
+                </tr>
+              </thead>
+              <tbody>
+
+              </tbody>
+            </table>
+          </rect-window>
+          <!-- <rect-window title="Receipt List"></rect-window>
+          <rect-window title="Item List"></rect-window> -->
       </div>
       <div class="col-2">
-          <rect-button title="Cust." background="#22aaee" icon="fa-solid fa-coffee">
+          <rect-button title="Cust." background="#22aaee">
               <template #icon><IconCustomer/></template>
           </rect-button>
-          <rect-button title="Receipt" background="#22aaee" icon="fa-solid fa-coffee"></rect-button>
-          <rect-button title="Item" background="#22aaee" icon="fa-solid fa-coffee"></rect-button>
-          <router-link to="/dashboard"><rect-button title="Back" background="#22aaee" icon="fa-solid fa-coffee" ></rect-button></router-link>
+          <rect-button title="Receipt" background="#22aaee" ></rect-button>
+          <rect-button title="Item" background="#22aaee"></rect-button>
+          <rect-button title="Back" background="#22aaee" @click="$router.push('/dashboard')"></rect-button>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-    import RectButton from '../components/elements/RectButton.vue';
-    import RectWindow from '../components/elements/RectWindow.vue';
-    import TheWelcome from '../components/TheWelcome.vue'
-    import IconCustomer from '../components/icons/IconCustomer.vue'
+<script setup>
+  import {ref, reactive} from "vue"
+  import axios from "axios"
+  import TheWelcome from '../components/TheWelcome.vue'
+  import IconCustomer from '../components/icons/IconCustomer.vue'
+
+  const title = ref("")
+  const records = reactive([])
+
+  const retrieveCustomerList = () => {
+    title.value = "Customer List"
+  }
+  const retrieveReceiptList = () => {
+    title.value = "Receipt List"
+  }
+  const retrieveItemList = () => {
+    title.value = "Item List"
+  }
+
+  const initialize = () => {
+    retrieveCustomerList()
+  }
+  initialize()
 </script>
